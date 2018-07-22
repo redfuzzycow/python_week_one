fa1d626d60d206534544836df234dc3bcebbb261
redfuzzycow <41201661+redfuzzycow@users.noreply.github.com>
2018-07-22 12:46:25 -0400

    Delete userdata.py

--- userdata.py
+++
@@ -1,7 +0,0 @@
-import sys
-with open('usernames.txt','r') as file:
-	for line in file:
-		creds = line.strip().split(':')
-		sys.stdout.write('-u '+creds[0]+' -p '+creds[1]+' ')
-		sys.stdout.flush()
-print
